ba82631c5ba8cf116dc3a1ef23baf73119f9d2c3
Victor Caldas <victor.solid3@gmail.com>
2020-03-07 09:39:57 -0300

    more function

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import initImc from "./js/calculoImc.js";
 import AddLista from "./js/addlista.js";
 import removeList from "./js/removeList.js";
+import initFilter from "./js/filter.js";
+import initAjax from "./js/pacientesJson.js";
 
 initImc();
 AddLista();
 removeList();
+initFilter();
+initAjax();
